e854abf86a4be37645f255b2493bbd17ddcafc7d
Dillan Suon <dillansuon@gmail.com>
2025-03-18 10:23:10 -0400

    removed vscoduim btop and ulauncher

--- VSCodium/User/History/-76e7e4b5/KBxG.js
+++
@@ -1,8 +0,0 @@
-// navbar.js
-
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('/partials/navbar.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('navbar').innerHTML = data)
-        .catch(error => console.error('Error loading navbar:', error));
-});
